5d6e77a965d5d0f717279d9580efa9014ae0c148
Luis F. Martínez Zarza <lfmartinez@atsistemas.com>
2019-05-03 12:56:01 +0200

    Empty project and initial commit.

---
+++ appletv-poc/appletv-poc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appletv-poc
+//
+//  Created by Luís Fernando Martínez Zarza on 03/05/2019.
+//  Copyright © 2019 Luís Fernando Martínez Zarza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
